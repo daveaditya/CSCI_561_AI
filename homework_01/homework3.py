9914d019b0d0469a7582fdbe7c9e66f74aafc0b0
Aditya Dave <connect@adityadave.dev>
2022-09-16 15:59:14 -0700

    initial population, fitness score calculation

--- homework_01/homework3.py
+++ homework_01/homework3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import sys
+import functools
+from math import factorial
+from itertools import permutations
 from typing import Callable, List, Tuple
 
 import numpy as np
@@ -9,13 +12,15 @@ from scipy.spatial.distance import euclidean
 # Type Hints
 City = Tuple[int, int, int]
 
-Genome = List[int]
-Population = List[Genome]
-PopulationFunc = Callable[[], Population]
-FitnessFunc = Callable[[Genome], int]
-SelectionFunc = Callable[[Population, FitnessFunc], Tuple[Genome, Genome]]
-CrossoverFunc = Callable[[Genome, Genome], Tuple[Genome, Genome]]
-MutationFunc = Callable[[Genome], Genome]
+Chromosome = List[int]  # represents one solution i.e. a single path in TSP
+Population = List[Chromosome]  # represents all the paths
+PopulationFunc = Callable[[], Population]  # A function that generates the population
+FitnessFunc = Callable[
+    [Chromosome, npt.NDArray[np.float64]], np.float64
+]  # A function that takes a chromosome and distance matrix to give fitness score of the chromosome
+SelectionFunc = Callable[[Population, FitnessFunc], Tuple[Chromosome, Chromosome]]
+CrossoverFunc = Callable[[Chromosome, Chromosome], Tuple[Chromosome, Chromosome]]
+MutationFunc = Callable[[Chromosome], Chromosome]
 
 
 def read_input(input_file_path: str) -> Tuple[int, List[City]]:
@@ -37,6 +42,17 @@ def read_input(input_file_path: str) -> Tuple[int, List[City]]:
     return city_count, cities
 
 
+def print_cities_for_path(cities: List[City], path: Chromosome) -> None:
+    """Print the list of cities in the order of visit
+
+    Args:
+        cities (List[City]): List of all cities
+        path (Genome): Path taken to visit all cities. Contains the index into the list of cities
+    """
+    for city_idx in path:
+        print(f"{cities[city_idx][0]}{cities[city_idx][1]}{cities[city_idx][2]}")
+
+
 def calculate_distances(n_cities: int, cities: List[City], distance_func: Callable) -> npt.NDArray[np.float64]:
     """Calculates the disance between each city.
 
@@ -55,32 +71,42 @@ def calculate_distances(n_cities: int, cities: List[City], distance_func: Callab
     return distance_matrix
 
 
-def print_cities_for_path(cities: List[City], path: List[int]) -> None:
-    """Print the list of cities in the order of visit
+def calculate_fitness_score(chromosome: Chromosome, distance_matrix: npt.NDArray[np.float64]) -> np.float64:
+    """Calculates the fitness score of a Chromosome using the distance matrix.
 
     Args:
-        cities (List[City]): List of all cities
-        path (List[int]): Path taken to visit all cities. Contains the index into the list of cities
+        chromosome (Chromosome): A possible path
+        distance_matrix (npt.NDArray[np.float64]): A matrix with call the distances for each city
+
+    Returns:
+        np.float64: Total cost / distance for the chromosome (path)
     """
-    for city_idx in path:
-        print(f"{cities[city_idx][0]}{cities[city_idx][1]}{cities[city_idx][2]}")
+    path_cost = 0.0
+    prev_city_idx = chromosome[0]
+    for idx in chromosome:
+        path_cost += distance_matrix[prev_city_idx][idx]
+        prev_city_idx = idx
+
+    return path_cost
 
 
 # a. initial population
-def create_initial_population(size: int, cities: List[int]):
+def create_initial_population(size: int, n_allele: int) -> Population:
     """Creates paths randomly or based on some heuristics.
 
     Args:
-        size (int): _description_
-        cities (List[int]): _description_
+        size (int): The number of members to create for the population
+        n_allele (n_allele): The number of values an allele can take
 
     Returns:
-        List[int]: initial population
+        Population: initial population
     """
-    initial_population: List[int] = list()
+    # a.1 Generate Randome Path / Genome
+    def generate_random_chromosome(size: int, n_allele: int):
+        random_chromosomes = list(permutations(range(n_allele)))[:size]
+        return list(map(lambda t: [*t, t[0]], random_chromosomes))
 
-    # TODO: logic here ...
-
+    initial_population: Population = generate_random_chromosome(size, n_allele)
     return initial_population
 
 
@@ -143,7 +169,15 @@ def main():
     distance_matrix = calculate_distances(n_cities, cities, euclidean)
     print("distance matrix .. ", distance_matrix)
 
-    pass
+    # Generate initial population
+    initial_population: Population = create_initial_population(size=4, n_allele=n_cities)
+    print("initial population ...", initial_population)
+
+    # Calculate fitness score for all the chromosomes
+    print(
+        "fitness scores ... ",
+        list(map(functools.partial(calculate_fitness_score, distance_matrix=distance_matrix), initial_population)),
+    )
 
 
 if __name__ == "__main__":
